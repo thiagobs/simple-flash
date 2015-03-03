a6d79dac5a5c02bfe26d6036251d022165e4c744
Thiago B. da Silva <thiago@agenciadeinternet.com>
2015-03-03 18:06:49 -0300

    First commit

---
+++ index.js
@@ -0,0 +1,16 @@
+module.exports = function(){
+    return function(req,res,next){
+        req.flash = function(type, message){
+            if(req.session){
+                req.session.messages = req.session.messages || [];
+                req.session.messages.push({type: type, message: message});
+            }
+        }
+        res.locals.flash = function(){
+            var messages = req.session.messages || [];
+            if(req.session && req.session.messages) delete req.session.messages;
+            return messages;
+        }
+        next();
+    }
+}
